027f8e1c2507c4effc9b443c846d711373002b67
celeste-rozario <celestearcosmeza@gmail.com>
2026-02-01 19:27:41 +1100

    Called weather data and time from API to html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 function updateWeather(response){
     let tempElement = document.querySelector("#temp-value");
     let temperature = response.data.temperature.current;
-    tempElement.innerHTML = Math.round(temperature);
+    let descriptionElement = document.querySelector("#description");
+    let descriptionData = response.data.condition.description;
+    let humidityElement = document.querySelector("#humidity");
+    let humidityData = response.data.temperature.humidity;
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let windSpeedData = response.data.wind.speed;
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
+    time.innerHTML = formatDate(date);
+    windSpeedElement.innerHTML = `${windSpeedData}km/hr`;
+    humidityElement.innerHTML = `${humidityData}%`;
+    cityElement.innerHTML = response.data.city;
+    tempElement.innerHTML = Math.round(temperature);
+    descriptionElement.innerHTML = descriptionData;
+}
+
+function formatDate(date){
+  
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10){
+        minutes = `0{minutes}`;
+    }
+
+return `${day} ${hours}:${minutes}`;
 }
 
 
@@ -17,10 +44,11 @@ function searchInput(event){
   event.preventDefault();
     let userInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#cityElement");
-    cityElement.innerHTML = userInput.value;
     searchCity(userInput.value);
 }
 
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchInput);
+
+searchCity("Melbourne");
